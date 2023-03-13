7982517b2acf7d98657f16f281b0c6e797f08591
hasancagrigungor <61599311+hasancagrigungor@users.noreply.github.com>
2023-03-13 11:02:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ hide_streamlit_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
-            .viewerBadge_container__1QSob {visibility: hidden;}
+            .viewerBadge_container__1QSob {visibility: hidden!important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
