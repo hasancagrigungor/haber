0b3a9bf848300cd38cb78c6321089529ddefb13f
hasancagrigungor <61599311+hasancagrigungor@users.noreply.github.com>
2023-03-16 11:45:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,50 +25,50 @@ def giris():
 if "key" not in st.session_state:
     giris()
 
+else:
+
+    st.button("Yenile")
+
+    ft.topluHaberEkle()
+    gunluk_trends=ft.trendsfull()
+    df=ft.haberGetir()
+
+    st.sidebar.title("Anlık Keyword")
+    gunluk_trends.reverse()
+    for a in gunluk_trends:
+        st.sidebar.error(a[0])
 
 
-st.button("Yenile")
+    dakika=round(df['kalan'].dt.total_seconds()/60)
+    sozluk = dict(zip(df['Başlık'],dakika))
 
-ft.topluHaberEkle()
-gunluk_trends=ft.trendsfull()
-df=ft.haberGetir()
-
-st.sidebar.title("Anlık Keyword")
-gunluk_trends.reverse()
-for a in gunluk_trends:
-    st.sidebar.error(a[0])
-
-
-dakika=round(df['kalan'].dt.total_seconds()/60)
-sozluk = dict(zip(df['Başlık'],dakika))
-
-col1,col2=st.columns([3,1])
-with col1:
-    st.subheader("Başlık")
-with col2:
-    st.subheader("Kalan Süre")
-
-for haber in sozluk.keys():
     col1,col2=st.columns([3,1])
     with col1:
-
-        st.write(haber)
+        st.subheader("Başlık")
     with col2:
+        st.subheader("Kalan Süre")
 
-        st.write(sozluk[haber],"dk")
-    st.markdown("""---""")
+    for haber in sozluk.keys():
+        col1,col2=st.columns([3,1])
+        with col1:
+
+            st.write(haber)
+        with col2:
+
+            st.write(sozluk[haber],"dk")
+        st.markdown("""---""")
 
 
-hide_streamlit_style = """
-            <style>
-            .viewerBadge_link__1S137 {display:none!important;}
-            .viewerBadge_link__1S137 {visibility: hidden!important;}
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            .viewerBadge_container__1QSob {visibility: hidden!important;}
-            
-           
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    hide_streamlit_style = """
+                <style>
+                .viewerBadge_link__1S137 {display:none!important;}
+                .viewerBadge_link__1S137 {visibility: hidden!important;}
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                .viewerBadge_container__1QSob {visibility: hidden!important;}
+
+
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
